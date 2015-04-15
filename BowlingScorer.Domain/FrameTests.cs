a0218866a34502159864e5526a054f8d180f7f35
Rusty B <mycoding.alert@gmail.com>
2015-04-14 20:05:39 -0500

    Initial commit.
    Added a basic Frame class.

---
+++ BowlingScorer.Domain/FrameTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScorer;
+
+namespace BowlingScorer.Domain
+{
+    [TestClass]
+    public class FrameTests
+    {
+        [TestMethod]
+        public void Result_OneRollOf10_ReturnsStrike()
+        {
+            var frame = new Frame(10);
+            Assert.AreEqual("X", frame.Result);
+        }
+
+        [TestMethod]
+        public void Result_TwoRollsSum10_ReturnsSpare()
+        {
+            var frame = new Frame(5, 5);
+            Assert.AreEqual("5/", frame.Result);
+        }
+
+        [TestMethod]
+        public void Result_TwoRollsSumLT10_ReturnsMiss()
+        {
+            var frame = new Frame(5, 4);
+            Assert.AreEqual("5-", frame.Result);
+        }
+
+    }
+}
---
+++ BowlingScorer/Frame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScorer
+{
+    public class Frame
+    {
+        public string Result
+        {
+            get
+            {
+                if (roll1 == 10)
+                {
+                    return "X";
+                }
+                else if ((roll1 + roll2) == 10)
+                {
+                    return string.Format("{0}/", roll1);
+                }
+                return string.Format("{0}-", roll1);
+            }
+        }
+
+        private int roll1;
+        private int roll2;
+
+        public Frame(int roll1) : this(roll1, 0) { }
+
+        public Frame(int roll1, int roll2)
+        {
+            this.roll1 = roll1;
+            this.roll2 = roll2;
+        }
+    }
+}
