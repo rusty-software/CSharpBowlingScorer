35c9bc12be09b10ae1ebfc022a2033b881b96dbb
Rusty B <mycoding.alert@gmail.com>
2015-04-15 20:32:04 -0500

    Added happy path tests for Final Frame
    Need to add sad path to prevent

---
+++ BowlingScorer.Tests/FinalFrameTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingScorer.Tests
+{
+    [TestClass]
+    public class FinalFrameTests
+    {
+        [TestMethod]
+        public void TwoRollsSumLT10_ScoresNoExtraRolls()
+        {
+            var rolls = new List<int> { 4, 5 };
+            var finalFrame = new FinalFrame(rolls);
+
+            Assert.AreEqual(9, finalFrame.RollSum);
+        }
+
+        [TestMethod]
+        public void TwoRollsSumTo10_ScoresOneExtraRoll()
+        {
+            var rolls = new List<int> { 4, 6, 7 };
+            var finalFrame = new FinalFrame(rolls);
+
+            Assert.AreEqual(17, finalFrame.RollSum);
+        }
+
+        [TestMethod]
+        public void FirstRollOf10_ScoresTwoExtraRolls()
+        {
+            var rolls = new List<int> { 10, 10, 1 };
+            var finalFrame = new FinalFrame(rolls);
+
+            Assert.AreEqual(21, finalFrame.RollSum);
+        }
+    }
+
+    public class FinalFrame
+    {
+        private List<int> rolls;
+
+        public int RollSum
+        {
+            get
+            {
+                return rolls.Sum();
+            }
+        }
+        public FinalFrame(List<int> rolls)
+        {
+            this.rolls = rolls;
+        }
+    }
+}
